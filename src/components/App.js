5f56ffa28074b4b02c2fe8dc3fcc7edd3a81c6f7
taylorvercillo <taylornvcasey@gmail.com>
2022-01-13 13:43:33 -0700

    almost done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import Navbar from '../components/NavBar.js'
+import Home from '../components/Home.js'
+import About from '..../components/About.js'
+
 function App() {
   return (
     <div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function NavBar() {
   return (
     <nav>
@@ -7,3 +8,5 @@ function NavBar() {
     </nav>
   );
 }
+
+export default NavBar
